673bd50fc04d5ea8f333b576cf990a1887391819
vinod <ajay@gmail.com>
2023-09-11 15:07:30 +0530

    ajay

---
+++ src/datatypes/TypesOfData.java
@@ -0,0 +1,28 @@
+package datatypes;
+
+public class TypesOfData {
+   public static void main(String[]args) {
+	   byte a = 100;
+	   System.out.println(a);
+	   short b=1234;
+	   System.out.println(b);
+	   int c= 1234567;
+	   long d=1234567891;
+	   char e = 'a';
+	   float f = 12.23f;
+	   double g = 66.23;
+	   boolean h = true;
+	    
+	   String s = "ajay";
+	   System.out.println(c);
+	   System.out.println(d);
+	   System.out.println(e);
+	   System.out.println(f);
+	   System.out.println(g);
+	   System.out.println(h);
+	   System.out.println(s);
+	   
+	   
+   }
+
+}
---
+++ src/datatypes/arthamaticOperators.java
@@ -0,0 +1,31 @@
+package datatypes;
+
+public class arthamaticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int a=10;
+		   int b=20;
+		  int add = a+b;
+		   System.out.println(add);
+
+		   int f= 10;
+		   double g= 2.5;
+	        double h=f-g;
+		   System.out.println(h);
+		   
+		   int aa=5;
+			int bb=4;
+			int multi=aa*bb;
+			System.out.println("[" + aa + " * " + bb + " = " + multi +" ] ");
+			
+			int cc= 8;
+			int dd= 2;
+			int quo=cc/dd;
+			System.out.println("{"+ cc + " / " + dd + " = " + quo + " } ");
+	         int rem=cc%dd;
+			System.out.println("{"+ cc + " % " + dd + " = " + rem + " } ");
+
+	}
+
+}
